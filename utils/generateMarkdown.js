8892a8fd7e3ea71f88b427c7bf775abc59aefe9f
irene <panisirene@gmail.com>
2023-05-30 18:31:22 -0700

    Edit content rendering in generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,7 @@ function renderLicenseLink(license) {
   switch(license) {
     case "MIT License":
       return "https://opensource.org/licenses/MIT";
-    case "GPL v3 (General Public License Version 3.0)":
+    case "General Public License Version 3.0 (GPL v3)":
       return "https://www.gnu.org/licenses/gpl-3.0";
     case "Apache License 2.0":
       return "https://opensource.org/licenses/Apache-2.0";
@@ -33,30 +33,27 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (!license) { return ""; }
-  return `
-    ## License
-    ${renderLicenseBadge(license)};
-  `;
+  return `## License\n
+This project is licensed under the ${license}.`;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-    # ${data.title}
-    ## Description
-    ${data.description}
-    ## Installation instructions
-    ${data.installation}
-    ## Usage
-    ${data.usage}
-    ## Contribution guidelines
-    ${data.contribution}
-    ## Test instructions
-    ${data.test}
-    ${renderLicenseSection($data.license)}
-    ## Questions
-    ${data.github} and ${data.email}
-  `;
+  return `# ${data.title}\n
+${renderLicenseBadge(data.license)}\n
+## Description\n
+${data.description}\n
+## Installation instructions\n
+${data.installation}\n
+## Usage\n
+${data.usage}\n
+## Contribution guidelines\n
+${data.contribution}\n
+## Test instructions\n
+${data.test}\n
+${renderLicenseSection(data.license)}\n
+## Questions\n
+For inquiries on this project, please contact me through GitHub @ [${data.github}](https://github.com/${data.github}) or via email at ${data.email}.`;
 }
 
 module.exports = {
